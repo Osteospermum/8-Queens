2287e349d1a086b9fd4827387d0210d51a81c7e3
Jacob <jacobjohnschnell@gmail.com>
2020-02-01 16:24:52 -0500

    Add project files.

---
+++ 8 Queens/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace _8_Queens
+{
+	partial class frmMain
+	{
+		/// <summary>
+		/// Variable nécessaire au concepteur.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Nettoyage des ressources utilisées.
+		/// </summary>
+		/// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Code généré par le Concepteur Windows Form
+
+		/// <summary>
+		/// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+		/// le contenu de cette méthode avec l'éditeur de code.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.lblSize = new System.Windows.Forms.Label();
+			this.updSize = new System.Windows.Forms.NumericUpDown();
+			this.btnExecute = new System.Windows.Forms.Button();
+			this.lblPossibilities = new System.Windows.Forms.Label();
+			this.lstPossibilities = new System.Windows.Forms.ListBox();
+			this.picBoard = new System.Windows.Forms.PictureBox();
+			this.updWait = new System.Windows.Forms.NumericUpDown();
+			this.lblWait = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.updSize)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.picBoard)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.updWait)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// lblSize
+			// 
+			this.lblSize.AutoSize = true;
+			this.lblSize.Location = new System.Drawing.Point(9, 9);
+			this.lblSize.Name = "lblSize";
+			this.lblSize.Size = new System.Drawing.Size(109, 13);
+			this.lblSize.TabIndex = 0;
+			this.lblSize.Text = "Largeur de grill (NxN):";
+			// 
+			// updSize
+			// 
+			this.updSize.Location = new System.Drawing.Point(12, 25);
+			this.updSize.Maximum = new decimal(new int[] {
+            16,
+            0,
+            0,
+            0});
+			this.updSize.Minimum = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+			this.updSize.Name = "updSize";
+			this.updSize.Size = new System.Drawing.Size(149, 20);
+			this.updSize.TabIndex = 1;
+			this.updSize.Value = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+			// 
+			// btnExecute
+			// 
+			this.btnExecute.Location = new System.Drawing.Point(12, 51);
+			this.btnExecute.Name = "btnExecute";
+			this.btnExecute.Size = new System.Drawing.Size(149, 23);
+			this.btnExecute.TabIndex = 2;
+			this.btnExecute.Text = "Exécuter";
+			this.btnExecute.UseVisualStyleBackColor = true;
+			this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+			// 
+			// lblPossibilities
+			// 
+			this.lblPossibilities.AutoSize = true;
+			this.lblPossibilities.Location = new System.Drawing.Point(9, 148);
+			this.lblPossibilities.Name = "lblPossibilities";
+			this.lblPossibilities.Size = new System.Drawing.Size(61, 13);
+			this.lblPossibilities.TabIndex = 3;
+			this.lblPossibilities.Text = "Possibilités:";
+			// 
+			// lstPossibilities
+			// 
+			this.lstPossibilities.FormattingEnabled = true;
+			this.lstPossibilities.Location = new System.Drawing.Point(12, 168);
+			this.lstPossibilities.Name = "lstPossibilities";
+			this.lstPossibilities.Size = new System.Drawing.Size(149, 329);
+			this.lstPossibilities.TabIndex = 4;
+			// 
+			// picBoard
+			// 
+			this.picBoard.Location = new System.Drawing.Point(167, 12);
+			this.picBoard.Name = "picBoard";
+			this.picBoard.Size = new System.Drawing.Size(485, 485);
+			this.picBoard.TabIndex = 5;
+			this.picBoard.TabStop = false;
+			this.picBoard.Click += new System.EventHandler(this.picBoard_Click);
+			this.picBoard.Paint += new System.Windows.Forms.PaintEventHandler(this.picBoard_Paint);
+			// 
+			// updWait
+			// 
+			this.updWait.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+			this.updWait.Location = new System.Drawing.Point(12, 109);
+			this.updWait.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+			this.updWait.Name = "updWait";
+			this.updWait.Size = new System.Drawing.Size(149, 20);
+			this.updWait.TabIndex = 7;
+			this.updWait.Value = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+			// 
+			// lblWait
+			// 
+			this.lblWait.AutoSize = true;
+			this.lblWait.Location = new System.Drawing.Point(9, 93);
+			this.lblWait.Name = "lblWait";
+			this.lblWait.Size = new System.Drawing.Size(110, 13);
+			this.lblWait.TabIndex = 6;
+			this.lblWait.Text = "Temps d\'attente (mS):";
+			// 
+			// frmMain
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(664, 509);
+			this.Controls.Add(this.updWait);
+			this.Controls.Add(this.lblWait);
+			this.Controls.Add(this.picBoard);
+			this.Controls.Add(this.lstPossibilities);
+			this.Controls.Add(this.lblPossibilities);
+			this.Controls.Add(this.btnExecute);
+			this.Controls.Add(this.updSize);
+			this.Controls.Add(this.lblSize);
+			this.Name = "frmMain";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.updSize)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.picBoard)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.updWait)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label lblSize;
+		private System.Windows.Forms.NumericUpDown updSize;
+		private System.Windows.Forms.Button btnExecute;
+		private System.Windows.Forms.Label lblPossibilities;
+		private System.Windows.Forms.PictureBox picBoard;
+		public System.Windows.Forms.ListBox lstPossibilities;
+		private System.Windows.Forms.NumericUpDown updWait;
+		private System.Windows.Forms.Label lblWait;
+	}
+}
+
---
+++ 8 Queens/Form1.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _8_Queens
+{
+	public partial class frmMain : Form
+	{
+		private int index;
+		private bool inProgress = false;
+
+		public frmMain()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+		}
+
+		private void picBoard_Paint(object sender, PaintEventArgs e)
+		{
+			int squareSize;
+			if (legalPositions != null)
+			{
+				squareSize = picBoard.Width / legalPositions.Count();
+				picBoard.Width = squareSize * legalPositions.Count();
+				picBoard.Height = squareSize * legalPositions.Count();
+			} else
+			{
+				squareSize = picBoard.Width / (int)updSize.Value;
+				picBoard.Width = squareSize * (int)updSize.Value;
+				picBoard.Height = squareSize * (int)updSize.Value;
+			}
+
+			int tempX = 0;
+			int tempY = 0;
+
+			while (tempX <= picBoard.Width)
+			{
+				tempY = 0;
+				while (tempY <= picBoard.Height)
+				{
+					if (tempX % (squareSize * 2) == 0)
+					{
+						if (tempY % (squareSize * 2) == 0)
+						{
+							e.Graphics.FillRectangle(Brushes.Black, tempX, tempY, squareSize, squareSize);
+						} else
+						{
+							e.Graphics.FillRectangle(Brushes.White, tempX, tempY, squareSize, squareSize);
+						}
+					} else
+					{
+						if (tempY % (squareSize * 2) == 0)
+						{
+							e.Graphics.FillRectangle(Brushes.White, tempX, tempY, squareSize, squareSize);
+						}
+						else
+						{
+							e.Graphics.FillRectangle(Brushes.Black, tempX, tempY, squareSize, squareSize);
+						}
+					}
+					tempY += squareSize;
+				}
+				tempX += squareSize;
+			}
+			if (solutions != null)
+			{
+				if (inProgress == false)
+				{
+					for (int i = 0; i < legalPositions.Length; i++)
+					{
+						e.Graphics.DrawImage(_8_Queens.Properties.Resources.queen, squareSize * solutions[index][i] + 5, squareSize * i + 5, squareSize - 10, squareSize - 10);
+					}
+				}
+				else
+				{
+					for (int i = 0; i < decisions.Count(); i++)
+					{
+						e.Graphics.DrawImage(_8_Queens.Properties.Resources.queen, squareSize * decisions[i] + 5, squareSize * i + 5, squareSize - 10, squareSize - 10);
+					}
+				}
+			}
+		}
+
+		private void btnExecute_Click(object sender, EventArgs e)
+		{
+			picBoard.Refresh();
+			solutions = new List<int[]>();
+			decisions = new List<int>();
+			int size = (int)updSize.Value;
+			legalPositions = new Boolean[size][];
+			for (int i = 0; i < size; i++)
+			{
+				legalPositions[i] = new Boolean[size];
+				for (int k = 0; k < size; k++)
+				{
+					legalPositions[i][k] = true;
+				}
+			}
+			for (int i = lstPossibilities.Items.Count - 1; i >= 0; i--)
+			{
+				lstPossibilities.Items.RemoveAt(i);
+			}
+			inProgress = true;
+			findSolutions();
+			inProgress = false;
+			lstPossibilities.Items.Add("Total: " + solutions.Count().ToString());
+			index = 0;
+			picBoard.Refresh();
+		}
+
+		private void picBoard_Click(object sender, EventArgs e)
+		{
+			if (solutions != null)
+			{
+				index++;
+				if (index >= solutions.Count())
+				{
+					index = 0;
+				}
+				picBoard.Refresh();
+			}
+		}
+
+		#region "Algorithm functions"
+
+		private Boolean[][] legalPositions;
+		public List<int[]> solutions;
+		private List<int> decisions;
+
+		private void FindNext(int rowNum)
+		{
+			int realRow = (rowNum - legalPositions.Length) * -1;
+			List<int> prevThisRow = new List<int>();
+			for (int i = 0; i < solutions.Count(); i++)
+			{
+				prevThisRow.Add(solutions[i][realRow]);
+			}
+			for (int i = 0; i < legalPositions.Length; i++)
+			{
+				UpdateLegal();
+				if (legalPositions[realRow][i] == true)
+				{
+					decisions.Add(i);
+					UpdateLegal();
+					if (updWait.Value > 0)
+					{
+						picBoard.Refresh();
+						System.Threading.Thread.Sleep((int)updWait.Value);
+					}
+					if (rowNum == 1)
+					{
+						solutions.Add(decisions.ToArray());
+						string s = "";
+						for (int k = 0; k < decisions.Count(); k++)
+						{
+							s += decisions[k].ToString("X");
+						}
+						lstPossibilities.Items.Add(s);
+						lstPossibilities.Refresh();
+						if (updWait.Value > 0)
+						{
+							System.Threading.Thread.Sleep(Math.Max(1000, (int)updWait.Value * 3));
+						}
+					}
+					else
+					{
+						FindNext(rowNum - 1);
+					}
+					decisions.Remove(i);
+					if (updWait.Value > 0)
+					{
+						picBoard.Refresh();
+						System.Threading.Thread.Sleep((int)updWait.Value);
+					}
+				}
+			}
+		}
+
+		private void UpdateLegal()
+		{
+			for (int i = 0; i < legalPositions.Length; i++)
+			{
+				for (int k = 0; k < legalPositions[i].Length; k++)
+				{
+					legalPositions[i][k] = true;
+				}
+			}
+			if (decisions.Count() > 0)
+			{
+				for (int i = 0; i < decisions.Count(); i++)
+				{
+					int currentParse = decisions[i];
+					Point testPos = new Point();
+					for (int k = 0; k < legalPositions.Length; k++)
+					{
+						legalPositions[i][k] = false;
+						legalPositions[k][currentParse] = false;
+						for (int mulX = -1; mulX <= 1; mulX += 2)
+						{
+							for (int mulY = -1; mulY <= 1; mulY += 2)
+							{
+								testPos = new Point(currentParse - k * mulX, i - k * mulY);
+								if (testPos.X < 0 || testPos.X >= legalPositions.Length || testPos.Y < 0 || testPos.Y >= legalPositions.Length)
+								{
+									break;
+								}
+								legalPositions[testPos.Y][testPos.X] = false;
+							}
+						}
+					}
+				}
+			}
+		}
+		public void findSolutions()
+		{
+			solutions = new List<int[]>();
+			FindNext(legalPositions.Length);
+			if (updWait.Value > 0)
+			{
+				System.Threading.Thread.Sleep(Math.Max(1000, (int)updWait.Value * 3));
+			}
+			decisions = new List<int>();
+		}
+
+		public void printLegalPos()
+		{
+			Console.WriteLine();
+			for (int i = 0; i < legalPositions.Length; i++)
+			{
+				for (int k = 0; k < legalPositions.Length; k++)
+				{
+					if (legalPositions[i][k] == true)
+					{
+						Console.Write('O');
+					}
+					else
+					{
+						Console.Write('X');
+					}
+				}
+				Console.WriteLine();
+			}
+		}
+		public void printBoard()
+		{
+			Console.WriteLine();
+			for (int i = 0; i < legalPositions.Length; i++)
+			{
+				for (int k = 0; k < legalPositions.Length; k++)
+				{
+					if (decisions.Count >= i + 1)
+					{
+						if (decisions[i] == k)
+						{
+							Console.Write('X');
+						}
+						else
+						{
+							Console.Write('O');
+						}
+					}
+					else
+					{
+						Console.Write('O');
+					}
+				}
+				Console.WriteLine();
+			}
+		}
+	}
+#endregion
+}
